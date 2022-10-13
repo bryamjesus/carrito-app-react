f5768f374882203465b183edac32a2c374d86167
Bryam Jesus Talledo Garcia <btalledog@odybank.com.pe>
2022-10-12 20:13:03 -0500

    Update EjemploPage.jsx

--- src/pages/EjemploPage.jsx
+++ src/pages/EjemploPage.jsx
@@ -1,7 +1,24 @@
+import { useEffect, useState } from "react";
+
 const EjemploPage = () => {
+  const [puerta, setPuerta] = useState(false);
+
+  //console.log('Primer Log')
+  useEffect(() => {
+    console.log('Primer useEffect')
+  })
+
+  //console.log('Segundo Log')
+  const handleButton = (parametro) => {
+    console.log(parametro)
+    setPuerta(!puerta)
+  }
+
   return (
     <div className="container">
       <h1>Ejemplo</h1>
+      <h4>Puerta {puerta ? 'abierta' : 'cerrada'}</h4>
+      <button onClick={() => handleButton('dsaf')}>{puerta ? 'Cerrar' : 'Abrir'}</button>
     </div>
   );
 }
